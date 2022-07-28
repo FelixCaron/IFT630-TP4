e14f100d52e59cf2c1975b238751ca02d8f47a1c
Felix Caron <felix.caron2@yahoo.ca>
2022-07-28 19:22:33 -0400

    WIP

--- tp4-base/src/server/main.cpp
+++ tp4-base/src/server/main.cpp
@@ -15,6 +15,7 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <pthread.h>
 
 using std::cout;
 using std::endl;
@@ -27,7 +28,7 @@ struct mesg_buffer {
     long mesg_type;
     int clientId;
     char mesg_text[100];
-} message;
+};
 
 // INFO for msgrcv msgsnd : https://linux.die.net/man/2/msgrcv
 // général: https://man7.org/linux/man-pages/man7/sysvipc.7.html
@@ -52,22 +53,27 @@ static mesg_buffer get_msg(key_t key) {
     // and returns identifier
     // voir les param
     msgid = msgget(key, 0666 | IPC_CREAT);
-
+    if(msgid != -1) {
+        
+    }
     // intéssant: https://www.ibm.com/docs/en/zos/2.3.0?topic=functions-msgrcv-message-receive-operation
     // en bref on pourra faire en sorte que le serveur reçoit tout mais send spécifiquemment a certaine client
     // avec un client ID ? (voir le param msgtyp )
-    if (msgrcv(msgid, &message, sizeof(message), 0, IPC_NOWAIT) != -1) {
-        printf("Data receive is : %s \n", message.mesg_text);
-        cout << "clientId:" + message.clientId << std::endl;
-
+        mesg_buffer message;
         strcpy(message.mesg_text, "this is a public test");
-        message.mesg_type = message.clientId;
+        //message.mesg_type = message.clientId; //client id is undefined here
         send_msg(1337, message);
     }; //
 
     return message;
 }
 
+static void read_msg(int msgid) {
+    mesg_buffer message;
+    msgrcv(msgid, &message, sizeof(message), 0, IPC_NOWAIT);
+    printf("Data receive is : %s \n", message.mesg_text);
+    cout << "ClientId:" << message.clientId << std::endl;
+}
 static void end_queue(key_t key) {
     cout << "Deleting queue :" + key << std::endl;
     // Supprimer a la fin !
@@ -85,7 +91,7 @@ void handle_signint(int sigNumber) {
 int main(int argc, char* argv[]) {
 	cout << "Server started" << endl;
 	// Association du signal avec la procédure de gestion (callback).
-    port = atoi(argv[1]);
+    //port = atoi(argv[1]);
     port = 1337;
 	signal(SIGINT, handle_signint);
     mesg_buffer leMessage = { 1 ,0, "nice text" };
