6acfe69f74d21e564085d0d4cf558415568157c7
bmw546 <marc-etienne-pepin@hotmail.com>
2022-07-28 16:31:43 -0400

    message queue now working ! with examplel !

--- tp4-base/src/client/main.cpp
+++ tp4-base/src/client/main.cpp
@@ -5,12 +5,67 @@
 
 #include "../../includes/signal.h"
 #include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
 using std::cout;
 using std::endl;
-int main(int argc, char* argv[]) {
 
-	// ...
+
+struct mesg_buffer {
+    long mesg_type;
+    int clientId;
+    char mesg_text[100];
+} message;
+
+// INFO for msgrcv msgsnd : https://linux.die.net/man/2/msgrcv
+// général: https://man7.org/linux/man-pages/man7/sysvipc.7.html
+
+static void send_msg(key_t key, mesg_buffer msg, int clientId) {
+    int msgid;
+
+    // msgget creates a message queue, ici on a enlever IPC_CREAT car on veut crash quand on as pas le bon data.
+    msgid = msgget(key, 0666);
+
+    // So the server can send back to us personally
+    msg.clientId = clientId;
+    // So just the server will listen to us (so no client id with 1337)
+    msg.mesg_type = 765554;
+
+    msgsnd(msgid, &msg, sizeof(msg), 0);
+}
+
+static mesg_buffer get_msg(key_t key, int clientId) {
+    int msgid;
+
+    // msgget creates a message queue, ici on a enlever IPC_CREAT car on veut crash quand on as pas le bon data.
+    msgid = msgget(key, 0666);
+
+    msgrcv(msgid, &message, sizeof(message), clientId, 0);
+
+    return message;
+}
+
+
+int main(int argc, char* argv[]) {
 	cout << "Client started" << endl;
+
+    int useId = atoi(argv[1]);
+    int defaultPort = atoi(argv[2]);
+    defaultPort = 1337;
+    mesg_buffer leMessage = { 1 , 0, "nice text" };
+
+    send_msg(defaultPort, leMessage, useId);
+
+    leMessage = get_msg(defaultPort, useId);
+    cout << leMessage.mesg_text << "\n";
+
 	return 0;
 }
--- tp4-base/src/server/main.cpp
+++ tp4-base/src/server/main.cpp
@@ -6,24 +6,92 @@
 #include "../../includes/pcslib.h"
 #include "../../includes/signal.h"
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
 using std::cout;
 using std::endl;
 typedef void Sigfunc(int);
 Sigfunc *signal(int, Sigfunc *);
 
+int port;
+
+struct mesg_buffer {
+    long mesg_type;
+    int clientId;
+    char mesg_text[100];
+} message;
+
+// INFO for msgrcv msgsnd : https://linux.die.net/man/2/msgrcv
+// général: https://man7.org/linux/man-pages/man7/sysvipc.7.html
+
+static void send_msg(key_t key, mesg_buffer msg) {
+    int msgid;
+
+    // msgget creates a message queue
+    // and returns identifier
+    msgid = msgget(key, 0666 | IPC_CREAT);
+
+    msgsnd(msgid, &msg, sizeof(msg), 0);
+
+    // // display the message
+    printf("Data send is : %s \n", msg.mesg_text);
+}
+
+static mesg_buffer get_msg(key_t key) {
+    int msgid;
+
+    // msgget creates a message queue
+    // and returns identifier
+    // voir les param
+    msgid = msgget(key, 0666 | IPC_CREAT);
+
+    // intéssant: https://www.ibm.com/docs/en/zos/2.3.0?topic=functions-msgrcv-message-receive-operation
+    // en bref on pourra faire en sorte que le serveur reçoit tout mais send spécifiquemment a certaine client
+    // avec un client ID ? (voir le param msgtyp )
+    if (msgrcv(msgid, &message, sizeof(message), 0, IPC_NOWAIT) != -1) {
+        printf("Data receive is : %s \n", message.mesg_text);
+        cout << "clientId:" + message.clientId << std::endl;
+
+        strcpy(message.mesg_text, "this is a public test");
+        message.mesg_type = message.clientId;
+        send_msg(1337, message);
+    }; //
+
+    return message;
+}
+
+static void end_queue(key_t key) {
+    cout << "Deleting queue :" + key << std::endl;
+    // Supprimer a la fin !
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+    msgctl(msgid, IPC_RMID, NULL);
+}
+
 // Procédure qui gère le signal.
 void handle_signint(int sigNumber) {
-	
+    end_queue(port);
+	exit(0);
 	// ...
 }
 
 int main(int argc, char* argv[]) {
 	cout << "Server started" << endl;
 	// Association du signal avec la procédure de gestion (callback).
+    port = atoi(argv[1]);
+    port = 1337;
 	signal(SIGINT, handle_signint);
-	
-	// ...
-	
-	exit(0);
+    mesg_buffer leMessage = { 1 ,0, "nice text" };
+
+	while (true) {
+        leMessage = get_msg(port);
+	}
+    exit(0);
 }
